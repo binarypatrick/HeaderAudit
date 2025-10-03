450ab03b0f23bb5455412da5fe433c4c14a23857
Patrick M <johndoe@example.com>
2025-10-02 22:53:09 -0400

    Improve with version information

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 This is a simple project that creates a webpage to return the request headers given as a JSON response.
 
-## Docker
+## Build
+
+```bash
+dotnet build ./src/BinaryPatrick.HeaderAudit.csproj -o out/
+dotnet ./out/src/BinaryPatrick.HeaderAudit.dll
+```
+
+```bash
+docker build -t header-audit -f ./src/Dockerfile ./src
+docker run -p 8080:8080 header-audit
+```
+
+## Docker Hub
 
 Image found at https://hub.docker.com/r/binarypatrick/header-audit
--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,22 @@
+using System.Reflection;
 using Microsoft.Extensions.Primitives;
 
-WebApplication app = WebApplication.CreateBuilder(args).Build();
+Console.WriteLine("***** Header Audit created by BinaryPatrick");
+Console.WriteLine("***** https://github.com/binarypatrick/headeraudit");
 
-app.Use(async (HttpContext context, Func<Task> next) =>
+var fileVersion = Assembly
+    .GetExecutingAssembly()
+    .GetCustomAttribute<AssemblyFileVersionAttribute>()?
+    .Version;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options => options.AddServerHeader = false);
+
+WebApplication app = builder.Build();
+app.Use(async (HttpContext context, Func<Task> _) =>
 {
+    context.Response.Headers.TryAdd("X-Developed-By", "https://github.com/binarypatrick/headeraudit");
+    context.Response.Headers.TryAdd("X-Version", fileVersion);
     Dictionary<string, StringValues> headers = context.Request.Headers.ToDictionary();
     await context.Response.WriteAsJsonAsync(new { RequestHeaders = headers });
 });
